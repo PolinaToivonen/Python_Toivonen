21bcb6aae63c23849022c808879e96f3bbcd1930
PolinaToivonen <44200765+PolinaToivonen@users.noreply.github.com>
2018-10-26 00:04:53 +0300

    Update caesar.py

--- homework01/caesar.py
+++ homework01/caesar.py
@@ -1,27 +1,23 @@
 def encrypt_caesar(plaintext: str) -> str:
     """
-    >>> encrypt_caesar("PYTHON")
-    'SBWKRQ'
-    >>> encrypt_caesar("python")
-    'sbwkrq'
-    >>> encrypt_caesar("Python3.6")
-    'Sbwkrq3.6'
-    >>> encrypt_caesar("")
-    ''
-    """
+        >>> encrypt_caesar("PYTHON")
+        'SBWKRQ'
+        >>> encrypt_caesar("python")
+        'sbwkrq'
+        >>> encrypt_caesar("Python3.6")
+        'Sbwkrq3.6'
+        >>> encrypt_caesar("")
+        ''
+        """
     ciphertext = ""
-    n = int()
     b = ord('a')
     c = ord('A')
     d = ord('X')
     e = ord('x')
-    f = ord('z')
-    g = ord('Z')
     for i in range(len(plaintext)):
-        n = ord(plaintext[i])
-        if ((n >= c) and (n <= g)) or ((n >= b) and (n <= f)):
-            if ((n >= d) and (n <= g)) or ((n >= e) and (n <= f)):
-                if (n >= d) and (n <= g):
+        if ('A' <= plaintext[i] <= 'Z') or ('a' <= plaintext[i] <= 'z'):
+            if ('X' <= plaintext[i] <= 'Z') or ('x' <= plaintext[i] <= 'z'):
+                if (plaintext[i] >= 'X') and (plaintext[i] <= 'Z'):
                     ciphertext += chr((ord(plaintext[i])) % d + c)
                 else:
                     ciphertext += chr((ord(plaintext[i])) % e + b)
@@ -34,29 +30,21 @@ def encrypt_caesar(plaintext: str) -> str:
 
 def decrypt_caesar(cipehrtext: str) -> str:
     """
-    >>> decrypt_caesar("SBWKRQ")
-    'PYTHON'
-    >>> decrypt_caesar("sbwkrq")
-    'python'
-    >>> decrypt_caesar("Sbwkrq3.6")
-    'Python3.6'
-    >>> decrypt_caesar("")
-    ''
-    """
+        >>> decrypt_caesar("SBWKRQ")
+        'PYTHON'
+        >>> decrypt_caesar("sbwkrq")
+        'python'
+        >>> decrypt_caesar("Sbwkrq3.6")
+        'Python3.6'
+        >>> decrypt_caesar("")
+        ''
+        """
     plaintext = ""
-    n = int()
-    b = ord('a')
     c = ord('A')
     d = ord('X')
-    e = ord('x')
-    f = ord('z')
-    g = ord('Z')
-    k = ord('C')
-    m = ord('c')
     for i in range(len(cipehrtext)):
-        n = ord(cipehrtext[i])
-        if ((n >= c) and (n <= g)) or ((n >= b) and (n <= f)):
-            if ((n >= c) and (n <= k)) or ((n >= b) and (n <= m)):
+        if ('A' <= cipehrtext[i] <= 'Z') or ('a' <= cipehrtext[i] <= 'z'):
+            if ('A' <= cipehrtext[i] <= 'C') or ('a' <= cipehrtext[i] <= 'c'):
                 plaintext += chr((ord(cipehrtext[i])) % c + d)
             else:
                 plaintext += chr(ord(cipehrtext[i]) - 3)
