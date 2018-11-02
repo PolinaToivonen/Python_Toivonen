f28c0bfc40d6a54604a79588728e5937d7882050
PolinaToivonen <44200765+PolinaToivonen@users.noreply.github.com>
2018-11-02 09:10:18 +0300

    Update caesar.py

--- homework01/caesar.py
+++ homework01/caesar.py
@@ -1,14 +1,14 @@
 def encrypt_caesar(plaintext: str) -> str:
     """
-        >>> encrypt_caesar("PYTHON")
-        'SBWKRQ'
-        >>> encrypt_caesar("python")
-        'sbwkrq'
-        >>> encrypt_caesar("Python3.6")
-        'Sbwkrq3.6'
-        >>> encrypt_caesar("")
-        ''
-        """
+    >>> encrypt_caesar("PYTHON")
+    'SBWKRQ'
+    >>> encrypt_caesar("python")
+    'sbwkrq'
+    >>> encrypt_caesar("Python3.6")
+    'Sbwkrq3.6'
+    >>> encrypt_caesar("")
+    ''
+    """
     ciphertext = ""
     for i in range(len(plaintext)):
         if ('A' <= plaintext[i] <= 'Z') or ('a' <= plaintext[i] <= 'z'):
@@ -26,15 +26,15 @@ def encrypt_caesar(plaintext: str) -> str:
 
 def decrypt_caesar(cipehrtext: str) -> str:
     """
-        >>> decrypt_caesar("SBWKRQ")
-        'PYTHON'
-        >>> decrypt_caesar("sbwkrq")
-        'python'
-        >>> decrypt_caesar("Sbwkrq3.6")
-        'Python3.6'
-        >>> decrypt_caesar("")
-        ''
-        """
+    >>> decrypt_caesar("SBWKRQ")
+    'PYTHON'
+    >>> decrypt_caesar("sbwkrq")
+    'python'
+    >>> decrypt_caesar("Sbwkrq3.6")
+    'Python3.6'
+    >>> decrypt_caesar("")
+    ''
+    """
     plaintext = ""
     for i in range(len(cipehrtext)):
         if ('A' <= cipehrtext[i] <= 'Z') or ('a' <= cipehrtext[i] <= 'z'):
