85db4e65ba5637adbb21f42e32d3fa08279a5cb4
PolinaToivonen <44200765+PolinaToivonen@users.noreply.github.com>
2018-11-02 09:08:58 +0300

    Update caesar.py

--- homework01/caesar.py
+++ homework01/caesar.py
@@ -10,17 +10,13 @@ def encrypt_caesar(plaintext: str) -> str:
         ''
         """
     ciphertext = ""
-    b = ord('a')
-    c = ord('A')
-    d = ord('X')
-    e = ord('x')
     for i in range(len(plaintext)):
         if ('A' <= plaintext[i] <= 'Z') or ('a' <= plaintext[i] <= 'z'):
             if ('X' <= plaintext[i] <= 'Z') or ('x' <= plaintext[i] <= 'z'):
                 if (plaintext[i] >= 'X') and (plaintext[i] <= 'Z'):
-                    ciphertext += chr((ord(plaintext[i])) % d + c)
+                    ciphertext += chr((ord(plaintext[i])) % ord('Х') + ord('А'))
                 else:
-                    ciphertext += chr((ord(plaintext[i])) % e + b)
+                    ciphertext += chr((ord(plaintext[i])) % ord('x') + ord('а'))
             else:
                 ciphertext += chr(3 + ord(plaintext[i]))
         else:
@@ -40,12 +36,10 @@ def decrypt_caesar(cipehrtext: str) -> str:
         ''
         """
     plaintext = ""
-    c = ord('A')
-    d = ord('X')
     for i in range(len(cipehrtext)):
         if ('A' <= cipehrtext[i] <= 'Z') or ('a' <= cipehrtext[i] <= 'z'):
             if ('A' <= cipehrtext[i] <= 'C') or ('a' <= cipehrtext[i] <= 'c'):
-                plaintext += chr((ord(cipehrtext[i])) % c + d)
+                plaintext += chr((ord(cipehrtext[i])) % ord('А') + ord('X'))
             else:
                 plaintext += chr(ord(cipehrtext[i]) - 3)
         else:
