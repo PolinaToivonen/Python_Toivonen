a4e4daef83767a4e7e125d6d2d7c84c090a65baa
PolinaToivonen <44200765+PolinaToivonen@users.noreply.github.com>
2019-02-26 14:14:29 +0300

    Update db.py

--- homework_06/db.py
+++ homework_06/db.py
@@ -2,7 +2,6 @@ from sqlalchemy import Column, String, Integer
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-'''from scraputils import get_news'''
 
 
 Base = declarative_base()
