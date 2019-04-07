8319a2f03468a1dae81f7da049f9bee3fba37692
PolinaToivonen <44200765+PolinaToivonen@users.noreply.github.com>
2019-04-07 13:34:58 +0300

    Create base.py

---
+++ homework_06/base.py
@@ -0,0 +1,16 @@
+from scraputils import get_news
+from db import News, session
+
+if __name__ == "__main__":
+    NEWS = get_news("https://news.ycombinator.com/newest", 34)
+    S = session()
+    for new in NEWS:
+        record = News(title=new['title'],
+                      author=new['author'],
+                      url=new['url'],
+                      comments=new['comments'],
+                      points=new['points'],
+                      cleaned=new['cleaned'])
+        S.add(record)
+    S.commit()
+    
