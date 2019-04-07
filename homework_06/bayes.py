6c0347bb3c11c1466cba69a209bd9b039a404d02
PolinaToivonen <44200765+PolinaToivonen@users.noreply.github.com>
2019-04-07 13:26:25 +0300

    Update bayes.py

--- homework_06/bayes.py
+++ homework_06/bayes.py
@@ -1,80 +1,60 @@
-from collections import Counter
+from collections import Counter, defaultdict
 from math import log
+from text import clean
 
 
 class NaiveBayesClassifier:
+    """ Naive Bayes classifier """
 
-    def __init__(self, alpha=1):
-        self.alpha = alpha
+    def __init__(self, alpha=1.0):
+        self.factor = alpha
+        self.classes = defaultdict(lambda: defaultdict(int))
+        self.words = defaultdict(lambda: defaultdict(int))
+        self.classes_counter = {}
+        self.words_counter = {}
+        self.pairs_counter = {}
 
     def fit(self, X, y):
         """ Fit Naive Bayes classifier according to X, y. """
-        labeled_words = []
-        for doc, label in zip(X, y):
-            for word in doc.split():
-                labeled_words.append((word, label))
-        self.word_lab_counted = dict(Counter(labeled_words))
-        self.labels_counted = dict(Counter(y))
-
-        words = [word for doc in X for word in doc.split()]
-        self.words_counted = dict(Counter(words))
-
-        self.info_labels = {}
-
-        for label in self.labels_counted:
-            self.info_labels[label] = {
-                'number of words': self.count_words_for_label(label),
-                'apr_prob': self.labels_counted[label] / len(y)
-            }
-        self.info_words = {}
+        all_words = []
+        pairs = []
+        self.classes_counter = dict(Counter(y))
+        for title, class_ in zip(X, y):
+            words_list = clean(title)
+            for word in words_list:
+                all_words.append(word)
+                pairs.append((word, class_))
+                self.classes[class_]['appearances'] += 1
+        for class_ in self.classes:
+            self.classes[class_]['prior'] = self.classes_counter[class_]/len(y)
+        self.words_counter = dict(Counter(all_words))
+        self.pairs_counter = dict(Counter(pairs))
+        d = len(self.words_counter)
+        for word in self.words_counter:
+            for class_ in self.classes:
+                self.words[word][class_] = (
+                    self.pairs_counter.get((word, class_), 0) + self.factor) / (self.classes[class_]['appearances'] + self.factor * d)
 
     def predict(self, X):
         """ Perform classification on an array of test vectors X. """
-        answers = []
-
-        for doc in X:
-            words = doc.split()
-            predictions = []
-
-            for label in self.info_labels:
-                apr_prob = self.info_labels[label]['apr_prob']
-                result = log(apr_prob)
-
-                for word in words:
-                    curr_word = self.info_words.get(word)
-                    if curr_word:
-                        result += log(curr_word[label])
-
-                predictions.append((result, label))
-
-            score, predicted = max(predictions)
-            answers.append(predicted)
-
-        return answers
+        classification = []
+        for title in X:
+            words_list = clean(title)
+            p_list = []
+            for class_ in self.classes:
+                ln_p_class = log(self.classes[class_]['prior'])
+                for word in words_list:
+                    if self.words[word][class_]:
+                        ln_p_class += log(self.words[word][class_])
+                p_list.append((class_, ln_p_class))
+            classification.append(max(p_list, key=lambda x: x[1]))
+        return classification
 
     def score(self, X_test, y_test):
         """ Returns the mean accuracy on the given test data and labels. """
-        total = len(y_test)
-        correct = 0
-        prediction = self.predict(X_test)
-
-        for i in range(len(prediction)):
-            if prediction[i] == y_test[i]:
-                correct += 1
-
-        return correct / total
-
-    def count_words_for_label(self, label):
-        c = 0
-        for word, word_label in self.word_lab_counted:
-            if word_label == label:
-                c += self.word_lab_counted[(word, word_label)]
-        return c
-
-    def smoothing(self, word, label):
-
-        alpha = self.alpha
-        n_ic = self.word_lab_counted.get((word, label), 0)
-        n_c = self.info_labels[label]['number_of_words']
-        d = len(self.words_counted)
-        return (n_ic + alpha) / (n_c + alpha * d)
+        count = 0
+        for record, label in zip(self.predict(X_test), y_test):
+            if record[0] == label:
+                count += 1
+        return count / len(y_test)
+    
